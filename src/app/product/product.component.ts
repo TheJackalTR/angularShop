c2d1bb121e662b292b7f07403325e4947110ce18
Aryen HEMVATAN <aryen.hemvatan@std.yeditepe.edu.tr>
2019-10-24 17:13:12 +0300

    another new commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from './product';
+declare let alertify: any;
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -9,12 +10,17 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title = "Ürün Listesi"
-  products: any[] = [
-    { id: 1, name: "Laptop", price: 2500, categoryId: 1, description: "Asus ZenBook" },
-    { id: 2, name: "Mouse", price: 25, categoryId: 2, description: "A4 Tech" }
+  filterText: string
+  products: Product[] = [
+    { id: 1, name: "Laptop", price: 2500, categoryId: 1, description: "Asus ZenBook", imageUrl: "https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" },
+    { id: 2, name: "Mouse", price: 25, categoryId: 2, description: "A4 Tech", imageUrl: "https://images.unsplash.com/photo-1563297007-0686b7003af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1091&q=80" }
   ]
 
   ngOnInit() {
+  }
+
+  addToCart(product) {
+    alertify.success(product.name + " added")
   }
 
 }
